498cbc870849a152cbc1107973c82ecac06a81d8
Ot Prat <43843799+OtPrat@users.noreply.github.com>
2022-09-12 15:07:29 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="luminance_analysis",
+    version="0.1",
+    author="Luigi Petrucco & Ot Prat",
+    author_email="lpetrucco@neuro.mpg.de",
+    packages=find_packages(),
+    install_requires=[
+        "ipython",
+        "numba",
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "scikit-image",
+        "ipywidgets",
+        "pandas"
+    ],
+)
